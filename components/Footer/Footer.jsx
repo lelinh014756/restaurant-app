41df21cf75e0482cff2c5808580fa2521f2aeed6
Hong Linh <linhle.070801@gmail.com>
2022-05-15 15:58:32 +0700

    upload Footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className="footer height-content">
       <div className="footer-item">
         <div className="footer-item-background">
           <Image
@@ -11,17 +11,51 @@ const Footer = () => {
             alt="background-footer"
             priority
             objectFit="cover"
-            width="500"
-            height="500"
+            layout="fill"
           />
         </div>
       </div>
       <div className="footer-item">
         <div className="footer-item-card">
-          <div className="footer-item-motto"></div>
+          <h3 className="footer-item-motto">
+            Oh yes, We did. The Gericht Restaurant, Well Backend slice of
+            Restaurant.
+          </h3>
         </div>
-        <div className="footer-item-card"></div>
-        <div className="footer-item-card"></div>
+        <div className="footer-item-card">
+          <h2 className="footer-item-title">Find our Restaurant</h2>
+          <p className="footer-item-text">
+            1654 R. Don Road #304.
+            <br /> NewYork, 85022
+            <br /> (602) 867-1010
+          </p>
+          <p className="footer-item-text">
+            2356 K. Laquie Rd #235.
+            <br /> NewYork, 85022
+            <br /> (602) 867-1010
+          </p>
+          <p className="footer-item-text">
+            1614 E. Erwin St #104.
+            <br /> NewYork, 85022
+            <br /> (602) 867-1010
+          </p>
+          <p className="footer-item-text">
+            1614 W. Caroll St #125.
+            <br /> NewYork, 85022
+            <br /> (602) 867-1010
+          </p>
+        </div>
+        <div className="footer-item-card">
+          <h2 className="footer-item-title">Working Hours</h2>
+          <p className="footer-item-text">
+            MONDAY UNTIL FRIDAY
+            <br /> 9:00 - 22:00
+          </p>
+          <p className="footer-item-text">
+            SATURDAY - SUNDAY
+            <br /> 12:00 - 24:00
+          </p>
+        </div>
       </div>
     </footer>
   );
